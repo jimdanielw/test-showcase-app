59c46a1759dd13490b8b6624d9a1618612f312db
Jim Daniels Wasswa <jim@regentmarkets.com>
2025-05-26 15:37:10 +0800

    chore: address code review comments

--- lib/src/deriv_chart/interactive_layer/crosshair/crosshair_widget.dart
+++ lib/src/deriv_chart/interactive_layer/crosshair/crosshair_widget.dart
@@ -58,7 +58,7 @@ class CrosshairWidget extends StatelessWidget {
         }
         return AnimatedBuilder(
           animation: crosshairZoomOutAnimation,
-          builder: (BuildContext context, _) {
+          builder: (_, __) {
             return RepaintBoundary(
               child: CrosshairArea(
                 mainSeries: mainSeries,
